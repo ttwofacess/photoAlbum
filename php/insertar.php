5c6fa8dfe2c4dbd69f5e741414c1be7364e852cc
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-07-11 17:46:17 -0300

    feat: creating the INSERT operation from PHP

--- php/insertar.php
+++ php/insertar.php
@@ -1,4 +1,14 @@
 <?php
 include 'datosConexion';
+$data = json_decode(file_get_contents("php://input"));
+
+if($data) {
+    $valores = json_encode($data->aGuardar);
+    $sql = "TRUNCATE TABLE albumes";
+    $conn->query($sql);
+    $sql = "INSERT INTO albumes (imags) VALUES ('$valores')";
+}
+
+$conn->close();
 
 ?>
